e78d73c6e34888f64afc22077a55b58290a844a4
Nikitabhagat27 <nikitabhagat004@gmail.com>
2024-10-07 15:56:00 +0530

    commit modified

--- LinkedListClient.java
+++ LinkedListClient.java
@@ -2,13 +2,14 @@ package LinnkedList;
 
 public class LinkedListClient {
 	public static void main(String[] args) {
-		SinglyLinkedList list = new SinglyLinkedList();
+		SingleyLinkedList list = new SingleyLinkedList();
 		list.addLastUsingTail(10);
 		list.addLastUsingTail(20);
 		list.addLastUsingTail(30);
 		list.addLastUsingTail(40);
 		list.addLastUsingTail(50);
 //		list.addFirst(70);
+		//list.addLastUsingTail(50);
 		list.addMid(4, 90);
 		list.print();
 		System.out.println();
