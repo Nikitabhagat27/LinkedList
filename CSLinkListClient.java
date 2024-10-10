fcd1be51cc1e065499e9dc2ad256e8a712a389dc
Nikitabhagat27 <nikitabhagat004@gmail.com>
2024-10-10 14:08:50 +0530

    modify

---
+++ CSLinkListClient.java
@@ -0,0 +1,15 @@
+package LinnkedList;
+
+
+
+public class CSLinkListClient {
+	public static void main(String[] args) {
+		CircularLinkedList cll = new CircularLinkedList();
+		cll.add(10);
+		cll.add(20);
+		cll.add(30);
+//		cll.print();
+		cll.print2();
+		System.out.println(cll.isCircularLinkedList());
+	}
+}
